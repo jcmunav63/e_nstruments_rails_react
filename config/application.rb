549b45726d5c1a8a8cf59045873bffbbb9dcd8c5
jcmunav63 <jcmunav63@gmail.com>
2024-05-04 20:05:13 -0600

    Rename repo to e_nstruments_rails_react.

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ require "action_cable/engine"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Rails_Newapp
+module E_Nstruments_Rails_React
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
