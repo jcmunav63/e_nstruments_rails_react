0dedfdd75409f32f6f63802009d0602ae1b3c394
jcmunav63 <jcmunav63@gmail.com>
2024-05-13 13:14:09 -0600

    Install React app in the frontend folder.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+// import guitar from './guitar.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Welcome to eNtruments Music Store App</h1>
+        <h2>Buy or sell your instruments in a breeze!</h2>
+      </div>
+      <div className="image">
+        {/* <img src={require('./images/guitar.png')} alt="Electric guitar"/> */}
+      </div>
     </div>
   );
 }
